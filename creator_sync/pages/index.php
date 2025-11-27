9ba423041bb87b444371bceccb522edd9bcaa54d
maicon vinicius <maiconvinicius7756@gmail.com>
2025-11-27 19:42:33 -0300

    Add files via upload
    Adicionando o script de criação do banco de dados, a estrutura de pastas e o arquivo de conexão.

---
+++ creator_sync/pages/index.php
@@ -0,0 +1,19 @@
+<?php>
+    include 'includes/conexao.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=<h1, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+
+        <body>
+        <h1> DEU CERTO! </h1>
+    </body>
+    
+</html>
