e25b6c274b80251ca10e368445171473d8e5316f
maicn <maiconvinicius7756@gmail.com>
2025-11-27 21:43:26 -0300

    Criação dos arquivos para criar, deletar e atualizar, e conexão com o banco feita.

---
+++ pages/index.php
@@ -0,0 +1,19 @@
+<?php
+    include 'C:\Users\994038\OneDrive - SENAC em Minas - EDU\PROJETOS\creator_sync\includes\conexao.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=<h1, initial-scale=1.0">
+        <title> teste </title>
+    </head>
+
+        <body>
+        <h1> Olá </h1>
+    </body>
+    
+</html>
