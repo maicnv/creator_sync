6d9eb2c20b6a7da7cbedbc49154054c8ad285c75
maicn <maiconvinicius7756@gmail.com>
2025-12-05 21:26:07 -0300

    Refazendo a estrutura principal de páginas e alguns arquivos.

--- includes/conexao.php
+++ includes/conexao.php
@@ -6,8 +6,9 @@
     $conn = new mysqli($host, $usuario, $senha, $banco);
 
     if ($conn->connect_error) {
-        die("Falha na conexão: " . $conn->connect_error);
+        die("Falha na conexão:  . $conn->connect_error);
     }
+        
 ?>
 
 
